454a8f95fa8ad52ba11b1089e2cf8d4f82b1091c
Jelle Druyts <jelle_druyts@yahoo.com>
2022-06-24 23:50:41 +0200

    Update support for well-known files

---
+++ src/ContosoMusiversity/Controllers/WellKnownController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using MicrosoftEntra.VerifiedId;
+
+namespace ContosoMusiversity.Controllers;
+
+public class WellKnownController : ControllerBase
+{
+    private readonly RequestClientOptions options;
+
+    public WellKnownController(RequestClientOptions options)
+    {
+        this.options = options;
+    }
+
+    [Route(".well-known/did.json")]
+    public string? GetDid()
+    {
+        return this.options.WellKnownDidJsonContents;
+    }
+
+    [Route(".well-known/did-configuration.json")]
+    public string? GetDidConfiguration()
+    {
+        return this.options.WellKnownDidJsonConfigurationContents;
+    }
+}
--- src/MicrosoftEntra.VerifiedId.Client/RequestClientOptions.cs
+++ src/MicrosoftEntra.VerifiedId.Client/RequestClientOptions.cs
@@ -1,12 +1,11 @@
-using System.Collections.Generic;
-
 namespace MicrosoftEntra.VerifiedId
 {
     public class RequestClientOptions
     {
         public string? Instance { get; set; } = "https://beta.did.msidentity.com/v1.0/"; // For Europe, use "https://beta.eu.did.msidentity.com/v1.0/".
         public string? Authority { get; set; } // The longform Decentralized Identifier (DID) of the issuer.
-        public IList<string>? DomainLinkageCredentials { get; set; } // The "linked_dids" JWT entries to include in the ".well-known/did-configuration.json" file for domain verification.
+        public string? WellKnownDidJsonContents { get; set; }
+        public string? WellKnownDidJsonConfigurationContents { get; set; }
         public string? TenantId { get; set; }
         public string? ClientName { get; set; }
     }
