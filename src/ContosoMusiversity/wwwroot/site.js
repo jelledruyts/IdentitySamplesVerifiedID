b48b6cfa8fc6e81c41989822bf85d170da523d09
Jelle Druyts <jelle.druyts@outlook.com>
2022-07-19 15:23:52 +0200

    Simplified web app javascript

--- src/ContosoMusiversity/wwwroot/site.js
+++ src/ContosoMusiversity/wwwroot/site.js
@@ -1,18 +1,7 @@
-﻿var qrCodeGenerator = new QRCode('qrCodeCanvas', { width: 300, height: 300 });
-
-var usePinCodeCheckbox = document.getElementById('usePinCode');
-var getCredentialButton = document.getElementById('getCredential');
-var errorMessageElement = document.getElementById('errorMessage');
-var responsePanel = document.getElementById('responsePanel');
-var qrCodeImage = document.getElementById('qrCodeImage');
-var qrCodeCanvas = document.getElementById('qrCodeCanvas');
-var deepLink = document.getElementById('deepLink');
-var requestIdElement = document.getElementById('requestId');
-var pinPanelElement = document.getElementById('pinPanel');
-var pinCodeElement = document.getElementById('pinCode');
-
-getCredentialButton.addEventListener('click', () => {
-    var data = { 'usePinCode': usePinCodeCheckbox.checked };
+﻿document.getElementById('getCredential').addEventListener('click', () => {
+    var data = {
+        'usePinCode': document.getElementById('usePinCode').checked
+    };
     fetch('api/issuer/issuance-request', {
         method: 'POST',
         headers: {
@@ -22,34 +11,30 @@ getCredentialButton.addEventListener('click', () => {
     }).then(function (response) {
         if (!response.ok) {
             // Something went wrong, show an error message.
+            var errorMessageElement = document.getElementById('errorMessage');
             response.json().then(function (errorBody) {
                 errorMessageElement.innerText = errorBody.title;
             }).catch(error => {
                 errorMessageElement.innerText = error.message;
             });
             errorMessageElement.style.display = 'block';
-            responsePanel.style.display = 'none';
+            document.getElementById('responsePanel').style.display = 'none';
         } else {
-            // The request was successful, show the response.
+            // The issuance request was successful, show the response.
             response.json().then(function (responseBody) {
-                requestIdElement.innerText = responseBody.requestId;
+                document.getElementById('requestId').innerText = responseBody.requestId;
+                var pinPanel = document.getElementById('pinPanel');
                 pinPanel.style.display = 'none';
                 if (responseBody.pinValue) {
-                    pinCodeElement.innerText = responseBody.pinValue;
+                    document.getElementById('pinCode').innerText = responseBody.pinValue;
                     pinPanel.style.display = 'block';
                 }
-                deepLink.href = responseBody.url;
-                if (responseBody.qrCode) {
-                    // The QR code is already available as a base64 encoded image.
-                    qrCodeImage.src = responseBody.qrCode;
-                    qrCodeImage.style.display = 'block';
-                } else {
-                    // Create a QR code from the URL.
-                    qrCodeGenerator.makeCode(responseBody.url);
-                    qrCodeCanvas.style.display = 'block';
-                }
-                errorMessageElement.style.display = 'none';
-                responsePanel.style.display = 'block';
+                document.getElementById('deepLink').href = responseBody.url;
+                var qrCodeImage = document.getElementById('qrCodeImage');
+                qrCodeImage.src = responseBody.qrCode;
+                qrCodeImage.style.display = 'block';
+                document.getElementById('errorMessage').style.display = 'none';
+                document.getElementById('responsePanel').style.display = 'block';
             })
         };
     }).catch(error => {
